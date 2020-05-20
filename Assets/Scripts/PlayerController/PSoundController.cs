25a03b248defe59957dc232231d1b1696e507107
craigmc08 <craigmc08@gmail.com>
2020-05-19 23:33:45 -0400

    Add basic sounds

---
+++ Assets/Scripts/PlayerController/PSoundController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource), typeof(Rigidbody), typeof(PDriverController))]
+public class PSoundController : MonoBehaviour
+{
+    AudioSource audioSource;
+    PDriverController controller;
+    Rigidbody rb;
+
+    [Range(0, 1)]
+    public float maxVolume = 1;
+    public float minPitch = 0.5f;
+    public float maxPitch = 1f;
+
+    [Range(0,1)]
+    public float engineSmoothing = 0.8f;
+
+    public float overspeedPitch = 2f;
+
+    public float timeForShift = 2f;
+
+    float timeNearMax = 0;
+
+    float previousFraction = 0;
+
+    void Start() {
+        audioSource = GetComponent<AudioSource>();
+        controller = GetComponent<PDriverController>();
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update() {
+        if (!controller.runPhysics) {
+            if (audioSource.isPlaying) audioSource.Stop();
+            return;
+        } else if (!audioSource.isPlaying) {
+            audioSource.volume = 0;
+            audioSource.Play();
+        }
+
+        var speed = Vector3.Project(rb.velocity, controller.KartForward).magnitude;
+        var speedFraction = speed / controller.maxForwardSpeed;
+
+        // var additionalFraction = timeNearMax / timeForShift;
+        // var additionalPitch = Mathf.LerpUnclamped(maxPitch, overspeedPitch, additionalFraction) - maxPitch;
+        float additionalPitch = 0;
+
+        var actualFraction = Mathf.Lerp(speedFraction, previousFraction, engineSmoothing);
+        
+        audioSource.volume = Mathf.Lerp(0, maxVolume, speedFraction);
+        audioSource.pitch = Mathf.LerpUnclamped(minPitch, maxPitch, actualFraction) + additionalPitch;
+
+        // if (speedFraction > 0.96f) {
+        //     timeNearMax += Time.deltaTime;
+        // } else if (timeNearMax > Time.deltaTime) {
+        //     timeNearMax -= Time.deltaTime;
+        // }
+
+        // if (timeNearMax >= timeForShift + Random.Range(-0.3f, 0.3f)) {
+        //     timeNearMax = Random.Range(-timeForShift, 0);
+        // }
+
+        previousFraction = actualFraction;
+    }
+}
--- Assets/Scripts/TimeTrialManager.cs
+++ Assets/Scripts/TimeTrialManager.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+[RequireComponent(typeof(AudioSource))]
 public class TimeTrialManager : MonoBehaviour
 {
     public static TimeTrialManager instance;
 
+    [Header("Settings")]
     public RaceProgress playerProgress;
     public PDriverController playerController;
     public float timeUntilLoadMenu = 5f;
 
+    [Header("Sounds")]
+    public AudioClip countdownSound;
+    public AudioClip lapCountSound;
+    public AudioClip finishSound;
+
+    [Header("Other Things")]
     public bool raceInProgress = false;
     public bool finishedRace = false;
     public int lap = 1;
@@ -19,6 +27,8 @@ public class TimeTrialManager : MonoBehaviour
     public float finishedTime = 0;
 
     public float finishedAt = 0;
+
+    AudioSource audioSource;
 
     private void Awake() {
         if (instance == null) {
@@ -30,6 +40,10 @@ public class TimeTrialManager : MonoBehaviour
         startAt = Time.time + 3f;
         playerController.controllable = false;
         playerController.runPhysics = false;
+
+        audioSource = GetComponent<AudioSource>();
+
+        audioSource.PlayOneShot(countdownSound);
     }
 
     private void Update() {
@@ -48,6 +62,9 @@ public class TimeTrialManager : MonoBehaviour
             return;
         }
 
+        var lastLap = lap;
+        var lastFinished = finishedRace;
+
         lap = playerProgress.laps + 1; // Player progress starts with 0 laps
         if (lap < 1) lap = 1;
 
@@ -58,7 +75,14 @@ public class TimeTrialManager : MonoBehaviour
             finishedRace = true;
             playerController.controllable = false;
             // Don't stop physics, let player slow down to a stop
+
             lap = 3;
+        }
+
+        if (finishedRace && !lastFinished) {
+            audioSource.PlayOneShot(finishSound);
+        } else if (lastLap != lap) {
+            audioSource.PlayOneShot(lapCountSound);
         }
     }
 
